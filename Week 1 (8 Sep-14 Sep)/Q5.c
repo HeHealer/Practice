eca322971ece075c3779e2fd45accd0cf5ab9ccb
Avishi Singhal <165550621+HeHealer@users.noreply.github.com>
2025-09-19 20:42:24 +0530

    Practice

--- Week 1 (8 Sep-14 Sep)/Q5.c
+++
@@ -1,9 +0,0 @@
-//Write a program too find wheather the entered number is even or odd.
-
-#include<stdio.h>
-int main(){
-    int a;
-    printf("Enter a number:");
-    scanf("%d",&a);
-    (a%2==0)?printf("The number is even"):printf("The number is odd");
-}
